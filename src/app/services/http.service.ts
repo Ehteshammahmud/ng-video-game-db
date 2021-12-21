ed320bdb83992e5f1f55fad1e3aa9c0a93891711
Ehteshammahmud <mmsamsdu@gmail.com>
2021-12-22 00:01:51 +0100

    Changes corrected

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -24,9 +24,4 @@ export class HttpService {
       params: params,
     });
   }
-  getGameDetails(id: string): Observable<Game> {
-    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
-    const gameTrailersRequest = this.http.get(
-      `${env.BASE_URL}/games/${id}/movies`
-
 }
