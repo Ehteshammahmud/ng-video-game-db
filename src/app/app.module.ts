219087e8ea5e52a0d81c794d51ca4a036b3eff1d
Ehteshammahmud <mmsamsdu@gmail.com>
2021-12-22 21:49:44 +0100

    Details component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,11 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
+import { DetailsComponent } from './components/details/details.component';
+import { TestComponent } from './component/test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent, TestComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
